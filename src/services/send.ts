ca515dc0aabe57c84684b26ad5effe0d8f57ed1b
adamstaveley <adam@motionwerk.com>
2018-08-08 11:11:10 +0200

    add debug logs for all requests

--- src/services/send.ts
+++ src/services/send.ts
@@ -1,8 +1,10 @@
 import * as request from 'request-promise-native';
 
 export default async (options: { method: string; uri: string; headers: { Authorization: string; }; body?: any; json?: boolean }): Promise<any> => {
+    console.debug('OCPI Request:', options);
     options.json = true;
     const result = await request(options);
+    console.debug('OCPI Response:', result);
     if (result.status_code === 1000) {
         return result.data;
     } else {
